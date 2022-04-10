49afef8f89657424574ea8ad13ff5a4bf53a1b03
tpyc12 <tpyc12@yandex.ru>
2022-04-10 16:20:40 +0300

    Create interface NewComponent.kt and inject Keyboard.kt, Mouse.kt, Monitor.kt

--- app/src/main/java/com/example/dependencyinjectionstart/example1/Activity.kt
+++ app/src/main/java/com/example/dependencyinjectionstart/example1/Activity.kt
@@ -1,10 +1,22 @@
 package com.example.dependencyinjectionstart.example1
 
+import javax.inject.Inject
+
 class Activity {
 
-    lateinit var computer: Computer
+    //    var keyboard: Keyboard = DaggerNewComponent.create().getKeyboard()
+//    var mouse: Mouse = DaggerNewComponent.create().getMouse()
+//    var monitor: Monitor = DaggerNewComponent.create().getMonitor()
+    @Inject
+    lateinit var keyboard: Keyboard
+
+    @Inject
+    lateinit var mouse: Mouse
+
+    @Inject
+    lateinit var monitor: Monitor
 
     init {
-        Component().inject(this)
+        DaggerNewComponent.create().inject(this)
     }
 }
