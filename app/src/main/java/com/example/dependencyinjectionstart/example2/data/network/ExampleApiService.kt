102294417daf439b6b66f0be5aa3a3c12a75cd05
tpyc12 <tpyc12@yandex.ru>
2022-04-10 18:10:54 +0300

    Fix providers on binds.

---
+++ app/src/main/java/com/example/dependencyinjectionstart/example2/data/network/ExampleApiService.kt
@@ -0,0 +1,17 @@
+package com.example.dependencyinjectionstart.example2.data.network
+
+import android.util.Log
+import javax.inject.Inject
+
+class ExampleApiService @Inject constructor() {
+
+    fun method() {
+        Log.d(LOG_TAG, "ExampleApiService")
+    }
+
+    companion object{
+
+        private const val LOG_TAG = "TAG"
+
+    }
+}
