afd4452ee40e6f559556e523c592bc3587eeda38
tpyc12 <tpyc12@yandex.ru>
2022-09-06 21:27:05 +0300

    feat: ApplicationComponent.kt feat: DataModule.kt with binds feat: DomainModule.kt with binds feat: inject all need classes

--- app/src/main/java/com/example/dependencyinjectionstart/example2/presentation/MainActivity.kt
+++ app/src/main/java/com/example/dependencyinjectionstart/example2/presentation/MainActivity.kt
@@ -1,15 +1,20 @@
 package com.example.dependencyinjectionstart.example2.presentation
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
 import com.example.dependencyinjectionstart.R
-import com.example.dependencyinjectionstart.example1.Activity
+import com.example.dependencyinjectionstart.example2.di.DaggerApplicationComponent
+import javax.inject.Inject
 
 class MainActivity : AppCompatActivity() {
 
+    @Inject
     lateinit var viewModel: ExampleViewModel
 
+    private val component = DaggerApplicationComponent.create()
+
     override fun onCreate(savedInstanceState: Bundle?) {
+        component.inject(this)
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         viewModel.method()
