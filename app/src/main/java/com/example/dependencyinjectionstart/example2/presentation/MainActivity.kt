596046046a2543e79ec466e16badfc440d8d18af
tpyc12 <tpyc12@yandex.ru>
2022-09-08 23:31:54 +0300

    feat: include inject view model

--- app/src/main/java/com/example/dependencyinjectionstart/example2/presentation/MainActivity.kt
+++ app/src/main/java/com/example/dependencyinjectionstart/example2/presentation/MainActivity.kt
@@ -3,6 +3,8 @@ package com.example.dependencyinjectionstart.example2.presentation
 import android.os.Bundle
 import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
 import com.example.dependencyinjectionstart.R
 import com.example.dependencyinjectionstart.example2.ExampleApp
 import com.example.dependencyinjectionstart.example2.di.DaggerApplicationComponent
@@ -11,7 +13,15 @@ import javax.inject.Inject
 class MainActivity : AppCompatActivity() {
 
     @Inject
-    lateinit var viewModel: ExampleViewModel
+    lateinit var viewModelFactory: ViewModelFactory
+
+    private val viewModel by lazy {
+        ViewModelProvider(this, viewModelFactory)[ExampleViewModel::class.java]
+    }
+
+    private val viewModel2 by lazy {
+        ViewModelProvider(this, viewModelFactory)[ExampleViewModel2::class.java]
+    }
 
     private val component by lazy {
         (application as ExampleApp).component
@@ -22,5 +32,6 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         viewModel.method()
+        viewModel2.method()
     }
 }
